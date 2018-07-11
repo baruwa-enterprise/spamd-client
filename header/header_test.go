75eff1cbe2dca377dbef366f04b6ba426cd57f07
Andrew Colin Kissa <andrew@topdog.za.net>
2018-07-11 13:28:01 +0200

    FET: Add tests

--- header/header_test.go
+++ header/header_test.go
@@ -6,3 +6,30 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 package header
+
+import "testing"
+
+type HeaderTestKey struct {
+	in  Header
+	out string
+}
+
+var NonExistant Header = 20
+
+var TestHeaders = []HeaderTestKey{
+	{Compress, "Compress"},
+	{User, "User"},
+	{ContentLength, "Content-length"},
+	{MessageClass, "Message-class"},
+	{Remove, "Remove"},
+	{Set, "Set"},
+	{NonExistant, ""},
+}
+
+func TestHeader(t *testing.T) {
+	for _, tt := range TestHeaders {
+		if s := tt.in.String(); s != tt.out {
+			t.Errorf("%q.String() = %q, want %q", tt.in, s, tt.out)
+		}
+	}
+}
--- request/request_test.go
+++ request/request_test.go
@@ -6,3 +6,84 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 package request
+
+import (
+	"testing"
+
+	"github.com/baruwa-enterprise/spamc/header"
+)
+
+type MethodTestKey struct {
+	in  Method
+	out string
+}
+
+type UsesHeaderTestKey struct {
+	in     Method
+	header header.Header
+	out    bool
+}
+
+type MsgTypeTestKey struct {
+	in  MsgType
+	out string
+}
+
+var (
+	NonExistantMethod  Method  = 20
+	NonExistantMsgType MsgType = 20
+)
+
+var TestMethods = []MethodTestKey{
+	{Check, "CHECK"},
+	{Headers, "HEADERS"},
+	{Ping, "PING"},
+	{Process, "PROCESS"},
+	{Report, "REPORT"},
+	{ReportIfSpam, "REPORT_IFSPAM"},
+	{Skip, "SKIP"},
+	{Symbols, "SYMBOLS"},
+	{Tell, "TELL"},
+	{NonExistantMethod, ""},
+}
+
+var TestUsesHeaders = []UsesHeaderTestKey{
+	{Check, header.Compress, true},
+	{Check, header.User, true},
+	{Check, header.ContentLength, true},
+	{Check, header.Remove, false},
+	{Check, header.Set, false},
+	{Check, header.MessageClass, false},
+	{Skip, header.MessageClass, false},
+}
+
+var TestMsgTypes = []MsgTypeTestKey{
+	{NoneType, ""},
+	{Ham, "ham"},
+	{Spam, "spam"},
+	{NonExistantMsgType, ""},
+}
+
+func TestMethod(t *testing.T) {
+	for _, tt := range TestMethods {
+		if s := tt.in.String(); s != tt.out {
+			t.Errorf("%q.String() = %q, want %q", tt.in, s, tt.out)
+		}
+	}
+}
+
+func TestUsesHeader(t *testing.T) {
+	for _, tt := range TestUsesHeaders {
+		if b := tt.in.UsesHeader(tt.header); b != tt.out {
+			t.Errorf("%q.UsesHeader(%q) = %t, want %t", tt.in, tt.header, b, tt.out)
+		}
+	}
+}
+
+func TestMsgType(t *testing.T) {
+	for _, tt := range TestMsgTypes {
+		if s := tt.in.String(); s != tt.out {
+			t.Errorf("%q.String() = %q, want %q", tt.in, s, tt.out)
+		}
+	}
+}
