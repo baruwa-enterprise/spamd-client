e9a8f4c7b8de79a2066ec8fabd09128b94291123
Andrew Colin Kissa <andrew@topdog.za.net>
2018-07-11 11:58:54 +0200

    FET: Implemented spamc compart cmdline tool

--- cmd/spamc/main.go
+++ cmd/spamc/main.go
@@ -8,18 +8,36 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"net"
+	"net/textproto"
 	"os"
+	"os/user"
 	"path"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/baruwa-enterprise/spamc"
+	"github.com/baruwa-enterprise/spamc/request"
+	"github.com/baruwa-enterprise/spamc/response"
 	flag "github.com/spf13/pflag"
 )
 
+const (
+	maxMsgSize      int64 = (256 * 1024 * 1024)
+	defaultUnixSock       = "/var/run/spamassassin/spamd.sock"
+)
+
 var (
-	cfg *Config
+	cfg     *Config
+	cmdName string
 )
 
 // Config represents the configuration flags
@@ -61,6 +79,7 @@ type Config struct {
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+	cmdName = path.Base(os.Args[0])
 	cfg = &Config{}
 	flag.StringSliceVarP(&cfg.Dest, "dest", "d", []string{"localhost"},
 		`Specify one or more hosts to connect to.
@@ -153,15 +172,303 @@ of stdout. This must be the last option. [Not Supported]`)
 
 func usage() {
 	fmt.Fprintf(os.Stderr, "SpamAssassin Client version %s\n\n", Version)
-	fmt.Fprintf(os.Stderr, "Usage: %s [options] [-e command [args]] < message\n", path.Base(os.Args[0]))
+	fmt.Fprintf(os.Stderr, "Usage: %s [options] [-e command [args]] < message\n", cmdName)
 	fmt.Fprint(os.Stderr, "\nOptions:\n")
 	flag.PrintDefaults()
 }
 
 func main() {
+	var err error
+	var line []byte
+	var u *user.User
+	var c *spamc.Client
+	var network, address string
+
 	flag.Usage = usage
 	flag.ErrHelp = errors.New("")
 	flag.CommandLine.SortFlags = false
 	flag.Parse()
-	// fmt.Printf("%v\n", cfg)
+	// msg cleared when in ping action
+
+	if cfg.User == "current user" {
+		u, err = user.LookupId(strconv.Itoa(os.Geteuid()))
+		if err != nil {
+			log.Fatal(err)
+		}
+		cfg.User = u.Username
+	}
+
+	if cfg.MaxSize > maxMsgSize {
+		usageErr("%s: -s parameter is beyond max of " + strconv.FormatInt(maxMsgSize, 10))
+	}
+
+	if cfg.LearnType != "" {
+		if cfg.LearnType != "ham" && cfg.LearnType != "spam" && cfg.LearnType != "forget" {
+			usageErr("%s: Please specify a legal learn type")
+		}
+		if cfg.Check {
+			usageErr("%s: Learning excludes check only")
+		}
+		if cfg.KeepAliceCheck {
+			usageErr("%s: Learning excludes ping")
+		}
+		if cfg.ReportIfSpam {
+			usageErr("%s: Learning excludes report if spam")
+		}
+		if cfg.Report {
+			usageErr("%s: Learning excludes report")
+		}
+		if cfg.Tests {
+			usageErr("%s: Learning excludes symbols")
+		}
+		if cfg.ReportType != "" {
+			usageErr("%s: Learning excludes reporting to collaborative filtering databases")
+		}
+	}
+
+	if cfg.ReportType != "" {
+		if cfg.ReportType != "report" && cfg.ReportType != "revoke" {
+			usageErr("%s: Please specify a legal report type")
+		}
+	}
+
+	if len(cfg.Dest) == 0 && cfg.UnixSocket == "" {
+		//None set, default to using default unix socket
+		if _, err = os.Stat(defaultUnixSock); os.IsNotExist(err) {
+			usageErr("%s: Please specify -d or -U")
+		}
+		network = "unix"
+		address = defaultUnixSock
+	} else if len(cfg.Dest) > 0 {
+		network = "tcp"
+		if cfg.UseIPv4 {
+			network = "tcp4"
+		}
+		if cfg.UseIPv6 {
+			network = "tcp6"
+		}
+		if network == "tcp" {
+			address = parseAddr(cfg.Dest[0], cfg.Port)
+		} else {
+			for _, addr := range cfg.Dest {
+				i := net.ParseIP(addr)
+				if i == nil {
+					address = parseAddr(addr, cfg.Port)
+					break
+				}
+				if network == "tcp6" && strings.Contains(addr, ":") {
+					address = parseAddr(addr, cfg.Port)
+					break
+				}
+				if network == "tcp4" && !strings.Contains(addr, ":") {
+					address = parseAddr(addr, cfg.Port)
+					break
+				}
+			}
+		}
+	}
+
+	// Read std input
+	m := []byte{}
+	r := bufio.NewReader(os.Stdin)
+	for {
+		line, err = r.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		m = append(m, line...)
+	}
+
+	// Create spamc client instance
+	c, err = spamc.NewClient(network, address, cfg.User, cfg.UseCompression)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.SetConnTimeout(time.Duration(cfg.ConnTimeOut) * time.Second)
+
+	// var retcode int
+	var success bool
+	var code response.StatusCode
+	var rs *response.Response
+	for i := 0; i < cfg.FilterRetry; i++ {
+		success = false
+		if cfg.Check {
+			rs, err = c.Check(m)
+			if err != nil {
+				code = response.ExSoftware
+			} else {
+				if rs.StatusCode == response.ExOK {
+					success = true
+					fmt.Printf("%.1f/%.1f\n", rs.Score, rs.BaseScore)
+				}
+				code = rs.StatusCode
+			}
+		} else if cfg.Tests {
+			c.EnableRawBody()
+			rs, err = c.Symbols(m)
+			if err != nil {
+				code = response.ExSoftware
+			} else {
+				if rs.StatusCode == response.ExOK {
+					success = true
+					fmt.Printf("%s", rs.Raw)
+				}
+				code = rs.StatusCode
+			}
+			c.DisableRawBody()
+		} else if cfg.ReportIfSpam {
+			c.EnableRawBody()
+			rs, err = c.ReportIfSpam(m)
+			if err != nil {
+				code = response.ExSoftware
+			} else {
+				if rs.StatusCode == response.ExOK {
+					success = true
+					fmt.Printf("%.1f/%.1f\n", rs.Score, rs.BaseScore)
+					fmt.Printf("%s", rs.Raw)
+				}
+				code = rs.StatusCode
+			}
+			c.DisableRawBody()
+		} else if cfg.Report {
+			c.EnableRawBody()
+			rs, err = c.Report(m)
+			if err != nil {
+				code = response.ExSoftware
+			} else {
+				if rs.StatusCode == response.ExOK {
+					success = true
+					fmt.Printf("%.1f/%.1f\n", rs.Score, rs.BaseScore)
+					fmt.Printf("%s", rs.Raw)
+				}
+				code = rs.StatusCode
+			}
+			c.DisableRawBody()
+		} else if cfg.HeadersOnly {
+			c.EnableRawBody()
+			rs, err = c.Headers(m)
+			if err != nil {
+				code = response.ExSoftware
+			} else {
+				if rs.StatusCode == response.ExOK {
+					success = true
+					fmt.Printf("%s", rs.Raw)
+					tp := textproto.NewReader(bufio.NewReader(bytes.NewReader(m)))
+					_, err = tp.ReadMIMEHeader()
+					if err == nil {
+						for {
+							tmp, e := tp.R.ReadBytes('\n')
+							if e != nil {
+								break
+							}
+							fmt.Printf("%s", tmp)
+						}
+					}
+				}
+				code = rs.StatusCode
+			}
+			c.DisableRawBody()
+		} else if cfg.LearnType != "" || cfg.ReportType != "" {
+			success, code = tell(c, m)
+		}
+		// retcode = int(code)
+		if success && code == response.ExOK || !success && !code.IsTemp() {
+			break
+		}
+		time.Sleep(time.Duration(cfg.RetrySleep) * time.Second)
+	}
+	// Exit with returned code
+	os.Exit(int(code))
+}
+
+func usageErr(s string) {
+	fmt.Fprintf(os.Stderr, s, cmdName)
+	flag.PrintDefaults()
+	os.Exit(int(response.ExUsage))
+}
+
+func parseAddr(a string, p int) (s string) {
+	i := net.ParseIP(a)
+	if i == nil {
+		s = fmt.Sprintf("%s:%s", a, strconv.Itoa(p))
+	} else {
+		s = fmt.Sprintf("[%s]:%s", a, strconv.Itoa(p))
+	}
+	return
+}
+
+// func check(c *spamc.Client, m []byte) (succeeded bool, code response.StatusCode) {
+// }
+
+func tell(c *spamc.Client, m []byte) (succeeded bool, code response.StatusCode) {
+	var err error
+	var h string
+	var l request.MsgType
+	var a request.TellAction
+	var r *response.Response
+	if cfg.LearnType != "" || cfg.ReportType != "" {
+		if cfg.LearnType == "spam" {
+			// learn as spam
+			l = request.Spam
+			a = request.LearnAction
+		} else if cfg.LearnType == "ham" {
+			// learn as ham
+			l = request.Ham
+			a = request.LearnAction
+		} else if cfg.LearnType == "forget" {
+			//forget
+			l = request.Spam
+			a = request.ForgetAction
+		}
+		if cfg.ReportType == "report" {
+			//report remote
+			l = request.Spam
+			a = request.ReportAction
+		} else if cfg.ReportType == "revoke" {
+			// revoke remote
+			l = request.Ham
+			a = request.RevokeAction
+		}
+		r, err = c.Tell(m, l, a)
+		if err != nil {
+			code = response.ExSoftware
+			return
+		}
+		code = r.StatusCode
+		if r.StatusCode != response.ExOK {
+			return
+		}
+		if cfg.LearnType != "" {
+			// learn
+			if cfg.LearnType == "forget" {
+				h = "Didremove"
+			} else {
+				h = "Didset"
+			}
+			if f := r.Headers.Get(h); f != "" {
+				fmt.Println("Message successfully un/learned")
+			} else {
+				fmt.Println("Message was already un/learned")
+			}
+			return
+		}
+		if cfg.ReportType != "" {
+			// Report
+			if cfg.ReportType == "revoke" {
+				h = "Didremove"
+			} else {
+				h = "Didset"
+			}
+			if f := r.Headers.Get(h); f != "" {
+				fmt.Println("Message successfully reported/revoked")
+			} else {
+				fmt.Println("Unable to report/revoke message")
+			}
+			return
+		}
+	}
+	return
 }
