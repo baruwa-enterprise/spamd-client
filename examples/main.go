9496749cff1f621cc21a69f81346b52e6e3ffa28
Andrew Colin Kissa <andrew@topdog.za.net>
2018-07-19 12:43:29 +0200

    PKG: Update pkg comments

--- examples/main.go
+++ examples/main.go
@@ -1,10 +1,12 @@
-// Spamc - Golang spamc client
 // Copyright (C) 2018 Andrew Colin Kissa <andrew@datopdog.io>
-//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
-// Package main
+
+/*
+Package main
+Spamc - Golang spamc client
+*/
 package main
 
 // StatusCode StatusCode
--- header/header_test.go
+++ header/header_test.go
@@ -1,10 +1,12 @@
-// Package header Golang spamc client
-// Spamc - Golang spamc client
 // Copyright (C) 2018 Andrew Colin Kissa <andrew@datopdog.io>
-//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
+
+/*
+Package header Golang spamc client
+Spamc - Golang spamc client
+*/
 package header
 
 import "testing"
