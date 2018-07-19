346af496aca642d7423a339214ccde2c137083f9
Andrew Colin Kissa <andrew@topdog.za.net>
2018-07-19 13:04:01 +0200

    PKG: Set the correct version

--- cmd/spamc/version.go
+++ cmd/spamc/version.go
@@ -1,21 +1,25 @@
-// Spamc - Golang spamc client
 // Copyright (C) 2018 Andrew Colin Kissa <andrew@datopdog.io>
-//
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
-// Package main
+
+/*
+Package main
+Spamc - Golang spamc client
+*/
 package main
 
-// The git commit that was compiled. This will be filled in by the compiler.
+// GitCommit is the git commit that was compiled.
+// This will be filled in by the compiler.
 var GitCommit string
 
-// The main version number that is being run at the moment.
-const Version = "0.1.0"
+// Version is the main version number that is being run at the moment.
+const Version = "0.0.1"
 
-// A pre-release marker for the version. If this is "" (empty string)
-// then it means that it is a final release. Otherwise, this is a pre-release
-// such as "dev" (in development)
+// VersionPrerelease is a pre-release marker for the version.
+// If this is "" (empty string) then it means that it is a final release.
+// Otherwise, this is a pre-release such as "dev" (in development)
 var VersionPrerelease = ""
 
+// BuildDate is the build date
 var BuildDate = ""
